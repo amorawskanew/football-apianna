60b16d582c5ae5c1d9960078be0a139e20a306c7
Anna Morawska <amorawska@yahoo.com>
2020-02-26 17:39:15 +0100

    commit

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 4000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World! I am tired ....'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
